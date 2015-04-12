aff1e248f0472f0d88c6278019a3fba3fa7f4e96
Boris Frenkel <fobos2k@gmail.com>
2015-04-12 13:26:22 +0300

    Полная отрисовка таблицы

--- main.js
+++ main.js
@@ -8,18 +8,57 @@ document.onreadystatechange = function()
 // Конструктор объекта CheckTable
 function CheckTable()
 {
-    document.getElementById('gadgetContent').innerHTML = '<h2>CheckTable -- кросстаблица с отметками</h2>';
-    
-    // Таблица
+    // DEBUG!!!
     var iColsNum = 3;
     var iRowsNum = 4;
-    for ( var i = 0; i < iRowsNum; i++ ) {     // Строки
-	document.getElementById('mainTable').innerHTML = '<tr>';
-	for ( var j = 0; j < iColsNum; j++ ) { // Колонки
-	    document.getElementById('mainTable').innerHTML = '<td>';
-	    document.getElementById('mainTable').innerHTML = i + ' - ' + j;
-	    document.getElementById('mainTable').innerHTML = '</td>';
-	}
-	document.getElementById('mainTable').innerHTML = '</tr>';
+
+    var mainTable = document.getElementById('mainTable');
+    
+    // Шапка таблицы
+    var newHeaderRow = document.createElement('tr');
+    newHeaderRow.id = 'headerRow';
+    for (var i = 0; i < iColsNum + 1; i++ ) {
+	var newHeaderCell = document.createElement('th');
+	newHeaderCell.id = 'header_' + i;
+	newHeaderCell.className = 'colHeader';
+	if ( i > 0) newHeaderCell.innerHTML = 'Конфигурация';
+	newHeaderRow.appendChild(newHeaderCell);
     }
+    mainTable.appendChild(newHeaderRow);
+    
+    // Тело таблицы
+    for ( var i = 0; i < iRowsNum; i++ ) {     // Строки
+	var newRow = document.createElement('tr');
+	newRow.id = 'row_' + i;
+	mainTable.appendChild(newRow);
+	
+	var newHeaderCell = document.createElement('td');
+	newHeaderCell.className = 'rowHeader';
+	newHeaderCell.innerHTML = 'Вып.';
+	newRow.appendChild(newHeaderCell);
+	for ( var j = 0; j < iColsNum; j++ ) { // Колонки
+	    var newCell = document.createElement('td');
+	    newCell.id = 'cell_' + i + j;
+	    newCell.className = 'cellCheck';
+	    newRow.appendChild(newCell);
+
+	    // Рисунок
+	    var imgCheck = document.createElement('img');
+	    imgCheck.src = 'img/no_check.png';
+	    imgCheck.id  = 'image_' + i + j;
+	    imgCheck.onClick = 'CheckChange(event)';
+	    newCell.appendChild(imgCheck);
+	}
+    }
+}
+
+function CheckChange (event)
+{
+  // получить объект событие.
+  // вместо event лучше писать window.event
+  event = event || window.event
+
+  // кросс-браузерно получить target
+  var t = event.target || event.srcElement
+  alert(t.className)
 }
